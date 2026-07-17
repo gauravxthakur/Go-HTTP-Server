d7b98ebcc616a3b94aa148dee0e2c9e5d180572d
gxt <gauravkthakur2002@gmail.com>
2026-07-18 04:19:10 +0530

    Add dynamic port configuration via flag package and SprintF formatting

--- main.go
+++ main.go
@@ -5,22 +5,28 @@ import(
 	"net/http"
 	"time"
 	"fmt"
+	"flag"
 )
 func main(){
+	var port int
+	flag.IntVar(&port, "port", 8080, "go backend server port")
+	flag.Parse()
+
 	app, err := app.NewApplication()
 
 	if err != nil{
 		panic(err)
 	}
-	app.Logger.Println("We are running our app")
 
 	http.HandleFunc("/health", HealthCheck)
 	server := &http.Server{
-		Addr: ":8080",
+		Addr: fmt.Sprintf(":%d", port),
 		IdleTimeout: time.Minute,
 		ReadTimeout: 10 * time.Second,
 		WriteTimeout: 30 * time.Second,
 	}
+
+	app.Logger.Printf("We are running on port %d\n", port)
 
 	err = server.ListenAndServe()
 	if err != nil{
