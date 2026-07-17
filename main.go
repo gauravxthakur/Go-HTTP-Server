e695b0932e25862dfa6af35a233ea9ddbfd5e1ba
gxt <gauravkthakur2002@gmail.com>
2026-07-18 03:43:59 +0530

    Initialise http server with health check endpoint

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import(
 	"github.com/gauravxthakur/go/internal/app"
+	"net/http"
+	"time"
+	"fmt"
 )
 func main(){
 	app, err := app.NewApplication()
@@ -10,4 +13,21 @@ func main(){
 		panic(err)
 	}
 	app.Logger.Println("We are running our app")
+
+	http.HandleFunc("/health", HealthCheck)
+	server := &http.Server{
+		Addr: ":8080",
+		IdleTimeout: time.Minute,
+		ReadTimeout: 10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	err = server.ListenAndServe()
+	if err != nil{
+		app.Logger.Fatal(err)
+	}
+}
+
+func HealthCheck(w http.ResponseWriter, r *http.Request){
+	fmt.Fprint(w, "Status is available\n")
 }
