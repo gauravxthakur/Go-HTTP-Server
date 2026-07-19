c366b21c9d0e937bf5b20552e0bbffbc39329b1f
gxt <gauravkthakur2002@gmail.com>
2026-07-19 21:08:07 +0530

    Create workout API handlers and add them to the Application struct

---
+++ internal/api/workout_handler.go
@@ -0,0 +1,34 @@
+package api
+
+import (
+	"net/http"
+	"github.com/go-chi/chi/v5"
+	"strconv"
+	"fmt"
+)
+
+type WorkoutHandler struct{}
+
+func NewWorkoutHandler() *WorkoutHandler{
+	return &WorkoutHandler{}
+}
+
+func (wh *WorkoutHandler) HandleGetWorkoutByID(w http.ResponseWriter, r *http.Request){
+	paramsWorkoutID := chi.URLParam(r, "workoutID")
+	if paramsWorkoutID == ""{
+		http.NotFound(w, r)
+		return
+	}
+
+	workoutID, err := strconv.ParseInt(paramsWorkoutID, 10, 64)
+	if err != nil{
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Workout ID is %d\n", workoutID)
+}
+
+func (wh *WorkoutHandler) HandleCreateWorkout(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "Created a workout\n")
+}
--- internal/app/app.go
+++ internal/app/app.go
@@ -9,13 +9,20 @@ import (
 
 type Application struct{
 	Logger *log.Logger
+	WorkoutHandler *api.WorkoutHandler
 }
 
 func NewApplication() (*Application, error){
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 
+	// our stores will go here
+
+	// our handlers will go here
+	workoutHandler := api.NewWorkoutHandler()
+
 	app := &Application{
 		Logger : logger,
+		WorkoutHandler: workoutHandler,
 	}
 	return app, nil
 }
