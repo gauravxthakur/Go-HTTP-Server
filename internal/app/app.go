ab484eb6460b0a93e073b51fcb0e644affddc3a2
gxt <gauravkthakur2002@gmail.com>
2026-07-23 21:34:00 +0530

    Integrate Goose in store.MigrateFS to execute migrations during NewApplication

--- internal/app/app.go
+++ internal/app/app.go
@@ -21,6 +21,12 @@ func NewApplication() (*Application, error){
 	if err != nil{
 		return nil, err
 	}
+
+	err = store.MigrateFS(pgDB, migrations.FS, ".")
+	if err != nil{
+		panic(err)
+	}
+
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 
 	// our stores will go here
